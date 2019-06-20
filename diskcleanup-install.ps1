bccff2929f1e5826e9bd42daa4494e7a973191f6
Pretend-Maintenance <46483681+Pretend-Maintenance@users.noreply.github.com>
2019-06-20 09:47:17 +0100

    Create diskcleanup-install.ps1

---
+++ diskcleanup-install.ps1
@@ -0,0 +1,3 @@
+Copy-Item "C:\Windows\winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe" -Destination “C:\Windows\System32”
+Copy-Item "C:\Windows\winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui" -Destination "C:\Windows\System32\en-US"
+cleanmgr.exe
